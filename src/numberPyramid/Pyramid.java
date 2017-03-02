a2baac010e71e32052bbf3431ba67c0936adb6f9
Joyce Chang <rejoyce@bu.edu>
2017-03-02 10:16:13 -0500

    Adding implementation

--- src/numberPyramid/Pyramid.java
+++ src/numberPyramid/Pyramid.java
@@ -5,4 +5,31 @@ import java.util.Scanner;
 
 public class Pyramid {
 	
+	private static int height;
+	
+	public static void main(String[] args){
+		Scanner s = new Scanner(System.in);
+		System.out.println("Enter pyramid height: ");
+		int num = s.nextInt();
+		height = num;
+		print(1);
+		s.close();
+		System.out.println("hello");
+	}
+	
+	
+	
+	private static void print(int num){
+		if (num <= height){
+			String[] str = new String[height];
+			Arrays.fill(str, 0, height-num, ".");
+			Arrays.fill(str, (height-num), height, Integer.toString(num));
+			System.out.println(Arrays.toString(str).
+					replace("[",  "").
+					replace("]", "").
+					replaceAll(", ",  ""));
+			num++;
+			print(num);
+		}
+	}
 }
